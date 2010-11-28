427798ca8f3dc91c952d5b9dce01087e95fc6ef2
Graham Daws <graham.daws@gmail.com>
2010-11-28 20:46:07 +0000

    Use lua's standard library function print to print values left the on stack after eval return. Pop the env table left on the stack as cleanup.

--- repl.cpp
+++ repl.cpp
@@ -23,6 +23,14 @@ int main(int, char**)
     lua_State * L = luaL_newstate();
     luaL_openlibs(L);
     
+    lua_getglobal(L, "print");
+    if(lua_type(L, -1) != LUA_TFUNCTION)
+    {
+        std::cerr<<"Startup error: _G['print'] is not a function"<<std::endl;
+        return 1;
+    }
+    int print_function_ref = luaL_ref(L, LUA_REGISTRYINDEX);
+    
     luaL_Reg cubescript_functions[] = {
         {"eval", eval},
         {NULL, NULL}
@@ -53,25 +61,29 @@ int main(int, char**)
             lua_rawgeti(L, LUA_REGISTRYINDEX, env_table_ref);
         else lua_pushvalue(L, LUA_GLOBALSINDEX);
         
-        lua_command_stack lua_command(L, lua_gettop(L));
+        int bottom = lua_gettop(L);
+        
+        lua_command_stack lua_command(L, bottom);
+        
+        int print_function = lua_command.push_command();
+        lua_rawgeti(L, LUA_REGISTRYINDEX, print_function_ref);
         eval_error error = eval(&code_c_str, code_c_str_end, lua_command);
         
         if(error && error.get_error_type() == EVAL_PARSE_ERROR)
             std::cout<<"Parse error: "<<error.get_description()<<std::endl;
         else if(error.get_error_type() == EVAL_RUNTIME_ERROR)
              std::cout<<"Runtime error: "<<error.get_description()<<std::endl;
-        else
-        {
-            std::string output;
-            if(lua_command.pop_string(output))
-                std::cout<<output<<std::endl;
-        }
+        
+        if(lua_gettop(L) > bottom + 1)
+            lua_command.call(print_function);
+        else lua_pop(L, 1);
+        
+        lua_pop(L, 1); // env_table_ref
         
         code.clear();
-        
         add_history(line);
     }
-       
+    
     std::cout<<std::endl;
     return 0;
 }
